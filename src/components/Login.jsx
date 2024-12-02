6cbd42df9c56dd894ce4e402096404cbcc083269
Nantawat <nantawattanasarn@gmail.com>
2024-12-02 13:13:26 +0700

    second commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -44,6 +44,17 @@ const Login = ({ setToken }) => {
               required
             />
           </div>
+          <div className="mb-3 min-w-72">
+            <p className="text-sm font-medium text-gray-700 mb-2">Password</p>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              className="rounded-md w-full px-3 py-2 border border-gray-300 outline-none"
+              type="password"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
           <button
             className="mt-2 w-full py-2 px-4 rounded-md text-white bg-black"
             type="submit"
@@ -58,3 +69,4 @@ const Login = ({ setToken }) => {
 };
 
 export default Login;
+
